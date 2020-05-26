215de42af310d47af6d2de4cc6f0efe831eabfa5
Denny James <dennyjames.k@gmail.com>
2020-05-26 20:57:30 +0530

    Update EP.sql

--- EP.sql
+++ EP.sql
@@ -1,6 +1,6 @@
 insert  into covid_status_dashboard(covid_status_dashboard_db_id, state, case_count, recovered_count, death_count, update_date) 
  
-(select nextval('covid_status_dashboard_db_id') covid_status_dashboard_db_id, 'All Inida' as state, sum(case_count) case_count, sum(recovered_count) recovered_count ,sum (death_count) death_count
+(select nextval('covid_status_dashboard_db_id') covid_status_dashboard_db_id, 'All India' as state, sum(case_count)+2970  case_count, sum(recovered_count) recovered_count ,sum (death_count) death_count
 ,update_date from covid_status_dashboard 
 where state <> 'All India'
 and update_date in
