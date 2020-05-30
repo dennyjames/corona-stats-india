9e2c62c251589f3970b6329e7d7ec343ae7c2222
Denny James <dennyjames.k@gmail.com>
2020-05-30 14:08:57 +0530

    Table structure changed

--- db.js
+++ db.js
@@ -9,16 +9,16 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     max: 5,
-    ssl: true
+    ssl: process.env.SSL
 });
 
 module.exports = {
 
     sql: {
-        insertQuery: "insert  into covid_status_dashboard(covid_status_dashboard_db_id, state, case_count, recovered_count, death_count, update_date) values (nextval('covid_status_dashboard_db_id'), $1, $2, $3, $4, date(now()))",
+        insertQuery: "insert  into covid_status_dashboard(covid_status_dashboard_db_id, state, case_count, recovered_count, death_count, update_date) values (nextval('covid_status_dashboard_db_id'), $1, $4, $2, $3, date(now()))",
         selectAllQuery: "select today.*, today.case_count-coalesce(lastday.case_count,0) as totaldelta, today.recovered_count-coalesce(lastday.recovered_count,0) as recovereddelta, today.death_count-coalesce(lastday.death_count,0) as deathdelta from ( select distinct c.state,c.case_count,c.recovered_count,c.death_count from covid_status_dashboard as c where c.update_date = (select distinct update_date from covid_status_dashboard order by update_date desc limit 1 offset 0) and c.case_count>0 ) as today left join ( select distinct c.state,c.case_count,c.recovered_count,c.death_count from covid_status_dashboard as c where c.update_date = (select distinct update_date from covid_status_dashboard order by update_date desc limit 1 offset 1)) as lastday on today.state=lastday.state order by today.case_count desc",
         ifExistQuery: "select count(1) as exists from covid_status_dashboard where update_date =  date(now()) and state = $1",
-        updateQuery: "update covid_status_dashboard set case_count = $2,recovered_count = $3 , death_count = $4 where update_date =  date(now()) and state = $1 ",
+        updateQuery: "update covid_status_dashboard set case_count = $4,recovered_count = $2 , death_count = $3 where update_date =  date(now()) and state = $1 ",
         selectTimeSeries: `select  sub.state,array_to_json(array_agg(row( to_char(sub.update_date, 'Mon DD, YYYY'), sub.case_count,(sub.case_count-(sub.recovered_count+sub.death_count)),sub.recovered_count,sub.death_count)order by update_date))as data from ( select * from covid_status_dashboard  order by update_date, state) as sub  group by  sub.state`
     },
     query: async (text, params) => {
--- index.js
+++ index.js
@@ -7,108 +7,117 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.get("/all/", async function (req, res) {
+app.get("/all/", async function(req, res) {
 
-    const result = await db.query(db.sql.selectAllQuery);
-    res.send(result.rows)
+	const result = await db.query(db.sql.selectAllQuery);
+	res.send(result.rows)
 });
 
-app.get("/timeseries/", async function (req, res) {
+app.get("/timeseries/", async function(req, res) {
 
-    const timeSeries = await db.query(db.sql.selectTimeSeries);
-    res.send(timeSeries.rows);
+	const timeSeries = await db.query(db.sql.selectTimeSeries);
+	res.send(timeSeries.rows);
 });
 
-app.listen(port, function () {
-    console.log("Your app is listening");
+app.listen(port, function() {
+	console.log("Your app is listening");
 });
 
 
 
 
 setInterval(async () => {
-    console.log("Refresh job started")
-    let response;
-    try {
-        response = await axios.get("https://www.mohfw.gov.in/");
-        if (response.status !== 200) {
-            console.log("ERROR");
-        }
-    } catch (err) {
-        return null;
-    }
+	console.log("Refresh job started")
+	let response;
+	try {
+		response = await axios.get("https://www.mohfw.gov.in/");
+		if (response.status !== 200) {
+			console.log("ERROR");
+		}
+	} catch (err) {
+		return null;
+	}
 
 
-    // get HTML and parse death rates
-    const html = cheerio.load(response.data);
+	// get HTML and parse death rates
+	const html = cheerio.load(response.data);
 
-    const table = html("#state-data > div > div > div > div > table > tbody").children("tr").toArray()
+	const table = html("#state-data > div > div > div > div > table > tbody").children("tr").toArray()
 
-    for (let row of table) {
-        let result = [];
-        const cells = row.children.filter(i => i.type === "tag");
-        if (cells.length === 4) {
+	for (let row of table) {
+		let result = [];
+		const cells = row.children.filter(i => i.type === "tag");
 
-            for (let j = 0; j < cells.length; j++) {
+		if (cells.length === 6) {
 
-                const customCell = cells[j].children.filter(i => i.type === "tag")[0];
-                //state name
-                if (j === 0) {
-                    result.push('All India');
-                }
-                // total_case
-                if (j === 1) {
-                    result.push(parseInt(customCell.children[0].data));
-                }
-                // recovered
-                if (j === 2) {
-                    result.push(parseInt(customCell.children[0].data));
-                }
-                //death
-                if (j === 3) {
-                    result.push(parseInt(customCell.children[0].data));
-                }
-            }
-        }
-        if (cells.length === 5 && cells[0].children.length>0) {
-            for (let j = 1; j < cells.length; j++) {
-                //state name
-                if (j === 1) {
-                    result.push(cells[j].children[0].data);
-                }
-                // total_case
-                if (j === 2) {
-                    result.push(parseInt(cells[j].children[0].data));
-                }
-                // recovered
-                if (j === 3) {
-                    result.push(parseInt(cells[j].children[0].data));
-                }
-                //death
-                if (j === 4) {
-                    result.push(parseInt(cells[j].children[0].data));
-                }
+			if (cells[0].children.length > 0) {
+				for (let j = 1; j < cells.length; j++) {
+					//state name
+					if (j === 1) {
+						result.push(cells[j].children[0].data);
+					}
+					// total_case
+					if (j === 5) {
+						result.push(parseInt(cells[j].children[0].data));
+					}
+					// recovered
+					if (j === 3) {
+						result.push(parseInt(cells[j].children[0].data));
+					}
+					//death
+					if (j === 4) {
+						result.push(parseInt(cells[j].children[0].data));
+					}
+				}
 
-            }
-        }
-        if(result.length ===4) {
-            try {
-                let rowCount = await db.query(db.sql.ifExistQuery, [result[0]]);
-                if (rowCount.rows[0] !== undefined && parseInt(rowCount.rows[0].exists) > 0)
+			}
 
-                    await db.query(db.sql.updateQuery, result);
-                else
+			else {
+				for (let j = 1; j < cells.length; j++) {
 
-                    await db.query(db.sql.insertQuery, result);
+					const customCell = cells[j].children.filter(i => i.type === "tag")[0];
+					//state name
+					if (j === 1) {
 
-            } catch (err) {
-                console.log(err.stack)
-            }
+						if (customCell!=undefined &&customCell.children[0].children[0].data.includes('Total')) {
+							result.push('All India');
+						}
+						else
+							break;
+					}
+					// total_case
+					if (j === 5) {
+						result.push(parseInt(customCell.children[0].data));
+					}
+					// recovered
+					if (j === 3) {
+						result.push(parseInt(customCell.children[0].data));
+					}
+					//death
+					if (j === 4) {
+						result.push(parseInt(customCell.children[0].data));
+					}
+				}
+			}
+		}
+		if (result.length === 4) {
+			try {
+				let rowCount = await db.query(db.sql.ifExistQuery, [result[0]]);
+				if (rowCount.rows[0] !== undefined && parseInt(rowCount.rows[0].exists) > 0)
 
-        }
+					await db.query(db.sql.updateQuery, result);
+				else
 
-    }
-    console.log("Refresh job finished")
+					await db.query(db.sql.insertQuery, result);
+
+			} catch (err) {
+				console.log(err.stack)
+			}
+
+		}
+
+	}
+	console.log("Refresh job finished")
 }, 600000);
 
 
